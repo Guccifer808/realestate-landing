8f6f32d365094216ef66b77931851fce85a0eb26
Guccifer808 <hucciallstars@gmail.com>
2023-11-30 16:47:03 +0200

    WA integration edits

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,8 @@ const App: FC<Props> = (props) => {
   }, []);
 
   const handleSubmit = () => {
-    // event.preventDefault();
-    window.open(
-      "https://api.whatsapp.com/send?phone=34644061559&text=%D0%94%D0%BE%D0%B1%D1%80%D1%8B%D0%B9%20%D0%B4%D0%B5%D0%BD%D1%8C%2C%20%D0%BC%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D0%BF%D0%BE%D0%BA%D1%83%D0%BF%D0%BA%D0%B0%20%D0%BD%D0%B5%D0%B4%D0%B2%D0%B8%D0%B6%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D0%B8%C2%A0%D0%B2%C2%A0%D0%98%D1%81%D0%BF%D0%B0%D0%BD%D0%B8%D0%B8"
-    );
+    const whatsappLink = "https://wa.link/f3lsz1";
+    window.location.href = whatsappLink;
   };
 
   const chatMessageWithBreak = `
